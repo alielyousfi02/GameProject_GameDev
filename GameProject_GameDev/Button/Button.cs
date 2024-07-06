21d3525838689b685272b29834c103a7b0939a52
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-06 15:53:34 +0200

    Add text to Button

--- GameProject_GameDev/Button/Button.cs
+++ GameProject_GameDev/Button/Button.cs
@@ -5,10 +5,13 @@ using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using System.Diagnostics;
+using GameProject_GameDev.Interfaces;
 
 namespace GameProject_GameDev.Button
 {
-    internal class Button
+    internal class Button : IGameObject
     {
         private int width;
         private int height;
@@ -16,24 +19,42 @@ namespace GameProject_GameDev.Button
         public Vector2 position;
         public Rectangle button;
 
+        private string text;
         private Texture2D texture;
         SpriteFont font;
+        private bool isClicked = false;
+        private bool isHovered = false;
 
-        public Button(int width, int height, Vector2 position, Texture2D texture)
+        public Button(int width, int height, Vector2 position, Texture2D texture, Color color, SpriteFont font, string text)
         {
             this.width = width;
             this.height = height;
             this.position = position;
-            button = new Rectangle(0, 0, width, height);
+            button = new Rectangle((int)position.X, (int)position.Y, width, height);
             this.texture = texture;
+            this.font = font;
+            this.text = text;
 
         }
         public void Draw(SpriteBatch spritebatch)
         {
             spritebatch.Draw(texture, button, Color.White);
+            Vector2 textSize = font.MeasureString(text);
+            Vector2 textPos = new Vector2(
+                button.X + (button.Width - textSize.X) / 2,
+                button.Y + (button.Height - textSize.Y) / 2);
+
+            spritebatch.DrawString(font, text, textPos, Color.Black);
         }
-        public void Update()
+        public void Update(GameTime gameTime)
         {
+            MouseState mouseState = Mouse.GetState();
+            Point mousePosition = new Point(mouseState.X, mouseState.Y);
+            if (button.Contains(mousePosition))
+            {
+                Debug.WriteLine("hover");
+            }
+
 
         }
 
