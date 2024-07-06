d3afb4a686dab2a8ee70e7a13a9822d167ae3b27
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-06 22:20:04 +0200

    Screen setting class

---
+++ GameProject_GameDev/UI/ScreenSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.UI
+{
+    public static class ScreenSettings
+    {
+        public static int ScreenWidth { get { return 960; } }
+        public static int ScreenHeight { get { return 480; } }
+    }
+}
