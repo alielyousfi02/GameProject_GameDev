a5ec6f025f7b7315996eea53e290cfa8bdf1389e
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-07 00:07:23 +0200

    Change location LevelBuilder + add Level and Level1

---
+++ GameProject_GameDev/Levels/Level.cs
@@ -0,0 +1,34 @@
+﻿using GameProject_GameDev.Levels.LevelBuilder;
+using GameProject_GameDev.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject_GameDev.Levels
+{
+    internal abstract class Level
+    {
+        protected Map map;
+        protected ContentManager content;
+
+        public Level(ContentManager content)
+        {
+            this.content = content;
+            map = new Map();
+        }
+
+        public virtual void LoadContent()
+        {
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(content.Load<Texture2D>("beachbackground"), new Rectangle(0, 0, ScreenSettings.ScreenWidth, ScreenSettings.ScreenHeight), Color.White);
+        }
+    }
+}
---
+++ GameProject_GameDev/Levels/Level1.cs
@@ -0,0 +1,51 @@
+﻿using GameProject_GameDev.Levels.LevelBuilder;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace GameProject_GameDev.Levels
+{
+    internal class Level1 : Level
+    {
+        public Level1(ContentManager content) : base(content)
+        {
+        }
+
+        public override void LoadContent()
+        {
+            base.LoadContent();
+            int[,] mapArray = new int[,]
+            {
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 }
+            };
+
+            //CollisionTiles.Content = content;
+
+            map.Generate(mapArray, 48); 
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+        }
+
+        public override void Draw(SpriteBatch spriteBatch)
+        {
+            base.Draw(spriteBatch);
+            map.Draw(spriteBatch);
+        }
+    }
+}
