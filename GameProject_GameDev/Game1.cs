3d24285bb5e3e8e5fc161666c21a271f823803ea
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-06 22:58:09 +0200

    MainMenu

--- GameProject_GameDev/Game1.cs
+++ GameProject_GameDev/Game1.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Xna.Framework;
+﻿using GameProject_GameDev.UI;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
@@ -8,6 +9,12 @@ namespace GameProject_GameDev
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        MainMenu main;
+        Texture2D texture;
+        Texture2D texture3;
+
+        Texture2D texture2;
+        SpriteFont font;
 
         public Game1()
         {
@@ -20,7 +27,23 @@ namespace GameProject_GameDev
         {
             // TODO: Add your initialization logic here
 
+            _graphics.IsFullScreen = false;
+            _graphics.PreferredBackBufferWidth = ScreenSettings.ScreenWidth;
+            _graphics.PreferredBackBufferHeight = ScreenSettings.ScreenHeight;
+            _graphics.ApplyChanges();
+            font = Content.Load<SpriteFont>("Text");
+            texture2= new Texture2D(GraphicsDevice, 1, 1);
+            texture3 = new Texture2D(GraphicsDevice, 1, 1);
+
+            texture2.SetData(new[] { Color.White });
+            texture3.SetData(new[] { Color.White });
+
+
+            texture = Content.Load<Texture2D>("background");
+            main = new MainMenu(texture, font, texture2, texture3);
             base.Initialize();
+
+
         }
 
         protected override void LoadContent()
@@ -38,12 +61,15 @@ namespace GameProject_GameDev
             // TODO: Add your update logic here
 
             base.Update(gameTime);
+            main.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
+            _spriteBatch.Begin();
+            main.Draw(_spriteBatch);
+            _spriteBatch.End();
             // TODO: Add your drawing code here
 
             base.Draw(gameTime);
---
+++ GameProject_GameDev/UI/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using GameProject_GameDev.Button;
+using GameProject_GameDev.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.UI
+{
+    internal class MainMenu : IGameObject
+    {
+        private StartButton startButton;
+        List<Button.Button> buttons;
+        private QuitButton quitButton;
+        private Texture2D texture;
+        private Rectangle rectangle;
+        public MainMenu(Texture2D texture, SpriteFont font, Texture2D but, Texture2D but2) //but is een tijdelijke oplossing
+        {
+            
+            this.texture = texture;
+            int Xpos = ScreenSettings.ScreenWidth / 2 - 50; // nog aanpassen
+            int Ypos = ScreenSettings.ScreenHeight / 2 - 15; // nog aanpassen
+            startButton = new StartButton(new Vector2(Xpos, Ypos),but, font, "Start");
+            quitButton = new QuitButton(new Vector2(Xpos, Ypos + 45), but2, font, "Quit");
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+          
+            spriteBatch.Draw(texture, new Rectangle(0, 0, ScreenSettings.ScreenWidth, ScreenSettings.ScreenHeight), Color.White);
+            startButton.Draw(spriteBatch);
+            quitButton.Draw(spriteBatch);
+
+
+        }
+
+        public void Update(GameTime time)
+        {
+            startButton.Update(time);
+            quitButton.Update(time);
+        }
+      
+    }
+
+}
