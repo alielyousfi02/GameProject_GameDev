4d1fbd9abdccfc016464c5575e00ccfa5ef09f2e
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-07 20:38:35 +0200

    Few interfaces

---
+++ GameProject_GameDev/Interfaces/IInputreader.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.Interfaces
+{
+    internal interface IInputreader
+    {
+        Vector2 ReadInput();
+    }
+}
