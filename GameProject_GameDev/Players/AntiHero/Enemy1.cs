1944d3e47545b7251d034ac0e60d1f19eabf1501
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-07 22:14:30 +0200

    Basic Enemy

---
+++ GameProject_GameDev/Players/AntiHero/Enemy1.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.Players.AntiHero
+{
+    internal class Enemy1 : WalkingEnemy
+    {
+        public Enemy1(Texture2D texture, Rectangle rectangle) : base(texture, rectangle)
+        {
+
+        }
+
+        public void Update(GameTime time)
+        {
+
+            base.Update(time);
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            base.Draw(spriteBatch);
+               
+        }
+    }
+}
---
+++ GameProject_GameDev/Players/AntiHero/WalkingEnemy.cs
@@ -0,0 +1,42 @@
+﻿using GameProject_GameDev.Interfaces;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.Players.AntiHero
+{
+    internal class WalkingEnemy : IGameObject
+    {
+
+
+        private Texture2D texture;
+        Animations.Animation animation;
+        private int width;
+        private int height;
+        private Rectangle rectangle;
+
+
+        public WalkingEnemy(Texture2D texture, Rectangle rectangle)
+        {
+            this.texture = texture;
+            animation = new Animations.Animation();
+            this.rectangle = rectangle;
+        }
+        public void Update(GameTime time)
+        {
+            rectangle = new Rectangle(rectangle.X, rectangle.Y, texture.Width, texture.Height); 
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, rectangle, Color.White);
+
+        }
+
+        
+    }
+}
