045087b7f8d83a23736c154c5cbc5aa7f3188e39
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-13 17:13:11 +0200

    Change way of collision

--- GameProject_GameDev/Players/Hero.cs
+++ GameProject_GameDev/Players/Hero.cs
@@ -8,84 +8,207 @@ using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Diagnostics;
 
 namespace GameProject_GameDev.Players
 {
-    internal class Hero : ICollisionEntity
+    internal class Hero
     {
-        private readonly IInputreader inputHandler;
-        private readonly ICollisionHandler collisionHandler;
-        private readonly Interfaces.IDrawable drawable;
-
-        public Vector2 Position { get; set; }
-        public Vector2 Velocity { get { return velocity; } set { velocity = value; } }
-        public Rectangle Rectangle { get; private set; }
-        public bool HasJumped { get; set; }
-
+        private Texture2D texture;
+        private Vector2 position;
         private Vector2 velocity;
-        private KeyboardState previousKeyboardState;
+        private Rectangle rectangle;
+        private Rectangle nonTransparentBoundingBox;
+        private bool hasJumped;
 
-        private const float gravity = 0.8f; 
-        private const float jumpVelocity = -15f; 
+        private float jumpVelocity = -9f;
+        private float gravity = 0.4f;
 
-        public Hero(Texture2D texture)
+        private readonly IInputreader inputReader;
+
+        public Vector2 Position
         {
-            inputHandler = new KeyBoardReader();
-            collisionHandler = new TileCollision();
-            drawable = new HeroDrawer(texture, this);
-
-            Position = new Vector2(64, 384);
-            Velocity = Vector2.Zero;
-            Rectangle = new Rectangle((int)Position.X, (int)Position.Y, texture.Width, texture.Height);
-            HasJumped = false;
-            previousKeyboardState = Keyboard.GetState();
+            get { return position; }
+            set { position = value; }
+        }
+        public Vector2 Velocity 
+        {
+            get { return velocity; } 
+            set { velocity = value; } 
+        }
+        public bool HasJumped
+        {
+            get { return hasJumped; }
+            set { hasJumped = value; }
         }
 
-        public void Update(GameTime gameTime)
+        public Hero(Texture2D texture, Vector2 startPosition)
         {
-            velocity = Velocity;
+            this.texture = texture;
+            this.position = startPosition;
+            this.velocity = Vector2.Zero;
+            this.hasJumped = false;
+            this.inputReader = new KeyBoardReader(); 
+        }
 
-            var inputVelocity = inputHandler.ReadInput(gameTime);
-            Position += new Vector2(inputVelocity.X, 0);
-            Rectangle = new Rectangle((int)Position.X, (int)Position.Y, Rectangle.Width, Rectangle.Height);
+        public void Load(ContentManager content)
+        {
+            nonTransparentBoundingBox = GetNonTransparentBoundingBox(texture);
+            rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        }
 
-            Jump();
+        private Rectangle GetNonTransparentBoundingBox(Texture2D texture)
+        {
+            Color[] textureData = new Color[texture.Width * texture.Height];
+            texture.GetData(textureData);
 
+            int left = texture.Width;
+            int right = 0;
+            int top = texture.Height;
+            int bottom = 0;
 
-            Position += new Vector2(0, Velocity.Y);
-            Rectangle = new Rectangle((int)Position.X, (int)Position.Y, Rectangle.Width, Rectangle.Height);
+            for (int y = 0; y < texture.Height; y++)
+            {
+                for (int x = 0; x < texture.Width; x++)
+                {
+                    Color pixel = textureData[y * texture.Width + x];
+                    if (pixel.A != 0)
+                    {
+                        left = Math.Min(left, x);
+                        right = Math.Max(right, x);
+                        top = Math.Min(top, y);
+                        bottom = Math.Max(bottom, y);
+                    }
+                }
+            }
+
+            if (left > right || top > bottom)
+            {
+                return new Rectangle(0, 0, 0, 0);
+            }
+
+            return new Rectangle(left, top, right - left + 1, bottom - top + 1);
+        }
+
+       
+        private void HandleInput(GameTime gameTime)
+        {
+            Vector2 input = inputReader.ReadInput(gameTime);
+            velocity.X = input.X;
+
+            if (input.Y < 0 && !hasJumped)
+            {
+                Jump();
+            }
         }
 
         private void Jump()
         {
-            var currentKeyboardState = Keyboard.GetState();
-            if (currentKeyboardState.IsKeyDown(Keys.Up) && previousKeyboardState.IsKeyUp(Keys.Up) && !HasJumped)
-            {
-                Velocity = new Vector2(Velocity.X, jumpVelocity);
-                HasJumped = true;
-            }
-            previousKeyboardState = currentKeyboardState;
+            velocity.Y = jumpVelocity;
+            hasJumped = true;
+        }
 
-            // Apply gravity
-            if (Velocity.Y < 10)
+        private void ApplyGravity()
+        {
+            if (hasJumped || position.Y < ScreenSettings.ScreenHeight - rectangle.Height)
             {
                 velocity.Y += gravity;
-                Velocity = velocity;
+                if (velocity.Y > 10)
+                {
+                    velocity.Y = 10;
+                }
             }
         }
 
-        public void HandleCollision(Rectangle newRectangle, int xOffset, int yOffset)
+        public void ResolveCollisions(Rectangle newRectangle, int xOffset, int yOffset)
         {
-            collisionHandler.HandleCollision(this, newRectangle, xOffset, yOffset);
-            if (Rectangle.TouchTopOf(newRectangle))
+            Rectangle adjustedRectangle = GetAdjustedRectangle();
+
+            if (adjustedRectangle.Intersects(newRectangle))
             {
-                HasJumped = false; 
+                Rectangle intersection = Rectangle.Intersect(adjustedRectangle, newRectangle);
+
+                if (intersection.Width < intersection.Height)
+                {
+                    if (adjustedRectangle.Center.X < newRectangle.Center.X)
+                    {
+                        position.X = newRectangle.Left - nonTransparentBoundingBox.Width - nonTransparentBoundingBox.X;
+                        Debug.WriteLine("left");
+                    }
+                    else
+                    {
+                        position.X = newRectangle.Right - nonTransparentBoundingBox.X;
+                        Debug.WriteLine("right");
+                    }
+                    velocity.X = 0f;
+                }
+                else
+                {
+                    if (adjustedRectangle.Center.Y < newRectangle.Center.Y && velocity.Y > 0)
+                    {
+                        position.Y = newRectangle.Top - nonTransparentBoundingBox.Height - nonTransparentBoundingBox.Y;
+                        velocity.Y = 0f;
+                        hasJumped = false;
+                        Debug.WriteLine("top");
+                    }
+                    else if (adjustedRectangle.Center.Y > newRectangle.Center.Y && velocity.Y < 0)
+                    {
+                        position.Y = newRectangle.Bottom - nonTransparentBoundingBox.Y;
+                        velocity.Y = 0f;
+                        Debug.WriteLine("bottom");
+                    }
+                }
             }
+
+            EnsureWithInterval(xOffset, yOffset);
+        }
+
+        private Rectangle GetAdjustedRectangle()
+        {
+            return new Rectangle(
+                (int)position.X + nonTransparentBoundingBox.X,
+                (int)position.Y + nonTransparentBoundingBox.Y,
+                nonTransparentBoundingBox.Width,
+                nonTransparentBoundingBox.Height
+            );
+        }
+
+        private void EnsureWithInterval(int xOffset, int yOffset)
+        {
+            if (position.X < 0) position.X = 0;
+            if (position.X > xOffset - nonTransparentBoundingBox.Width) position.X = xOffset - nonTransparentBoundingBox.Width;
+            if (position.Y < 0) velocity.Y = 1f;
+            if (position.Y > yOffset - nonTransparentBoundingBox.Height) position.Y = yOffset - nonTransparentBoundingBox.Height;
+        }
+
+
+
+
+        public void Update(GameTime gameTime)
+        {
+            HandleInput(gameTime);
+
+            ApplyGravity();
+
+            position += velocity;
+
+            //if (Math.Abs(velocity.X) < 0.01f) velocity.X = 0f;
+            //if (Math.Abs(velocity.Y) < 0.01f) velocity.Y = 0f;
+
+            rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
         }
 
         public void Draw(SpriteBatch spriteBatch)
         {
-            drawable.Draw(spriteBatch);
+            Texture2D redTexture = new Texture2D(spriteBatch.GraphicsDevice, 1, 1);
+            redTexture.SetData(new[] { Color.Red });
+
+            Rectangle adjustedRectangle = GetAdjustedRectangle();
+
+            // Uncomment the following line for debugging purposes
+            // spriteBatch.Draw(redTexture, adjustedRectangle, Color.White);
+
+            spriteBatch.Draw(texture, rectangle, Color.White);
         }
     }
 }
