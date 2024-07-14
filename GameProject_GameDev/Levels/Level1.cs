3904fc98c456573d5a58cce260b0d33ffda79459
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-14 23:52:10 +0200

    Update Level1

--- GameProject_GameDev/Levels/Level1.cs
+++ GameProject_GameDev/Levels/Level1.cs
@@ -4,24 +4,29 @@ using GameProject_GameDev.Players.AntiHero;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System.Diagnostics;
 
 namespace GameProject_GameDev.Levels
 {
     internal class Level1 : Level
     {
+
         private Hero hero;
         private Enemy1 enemy1;
+        StandingEnemy standingEnemy;
         public Level1(ContentManager content) : base(content)
         {
-            hero = new Hero(content.Load<Texture2D>("HERO"));
-
-            enemy1 = new Enemy1(content.Load<Texture2D>("HERO"), new Rectangle(500, 500, 48, 48));
+            hero = new Hero( new Vector2(500, 100));
+            standingEnemy = new StandingEnemy(content.Load<Texture2D>("standing"), new Vector2(400, 602));
+            enemy1 = new Enemy1(content.Load<Texture2D>("HERO2"), new Vector2(1146,432),100);
         }
 
         public override void LoadContent()
         {
             base.LoadContent();
             CollisionTiles.Content = content;
+            hero.Load(content);
 
             int[,] mapArray = new int[,]
             {
@@ -34,11 +39,11 @@ namespace GameProject_GameDev.Levels
                 { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
                 { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
                 { 2,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
-                { 2,1,1,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
                 { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
-                { 2,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,1,1,1,1,2 },
                 { 2,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
-                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2 },
+                { 2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,2 },
                 { 2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2 }
             };
 
@@ -51,7 +56,7 @@ namespace GameProject_GameDev.Levels
             enemy1.Update(gameTime);
             foreach (CollisionTiles item in map.CollisionTiles)
             {
-                hero.HandleCollision(item.Rectangle, map.Width, map.Height);
+                hero.ResolveCollisions(item.Rectangle, map.Width, map.Height);
             }
             base.Update(gameTime);
         }
@@ -60,6 +65,7 @@ namespace GameProject_GameDev.Levels
         {
             base.Draw(spriteBatch);
             map.Draw(spriteBatch);
+            standingEnemy.Draw(spriteBatch);
             enemy1.Draw(spriteBatch);
             hero.Draw(spriteBatch);
         }
