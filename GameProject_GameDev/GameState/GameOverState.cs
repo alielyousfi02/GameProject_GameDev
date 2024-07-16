aea07d646433f5abe92a47a9fd34f2f35228251b
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-16 19:37:19 +0200

    Added GameOverState

---
+++ GameProject_GameDev/GameState/GameOverState.cs
@@ -0,0 +1,42 @@
+﻿using GameProject_GameDev.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.GameState
+{
+    internal class GameOverState : State
+    {
+        private Texture2D gameovertexture;
+        public GameOverState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            gameovertexture = content.Load<Texture2D>("gameover");
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(gameovertexture, new Rectangle(0, 0, ScreenSettings.ScreenWidth, ScreenSettings.ScreenHeight), Color.White);
+
+        }
+
+        public override void LoadContent()
+        {
+            
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+
+        }
+    }
+}
