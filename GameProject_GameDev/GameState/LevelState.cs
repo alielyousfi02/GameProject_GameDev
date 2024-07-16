e0d7f55f72f7293d94d935f4d12aea9a4e861a78
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-16 19:24:08 +0200

    Add LevelState Update Menustate

---
+++ GameProject_GameDev/GameState/LevelState.cs
@@ -0,0 +1,54 @@
+﻿using GameProject_GameDev.Levels;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.GameState
+{
+    internal class LevelState : State
+    {
+        private Level1 level1;
+        private Level curLevel;
+
+        public LevelState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            level1 = new Level1(content);
+            curLevel = level1;
+        }
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            curLevel.Draw(spriteBatch);
+        }
+
+        public override void LoadContent()
+        {
+            curLevel.LoadContent();
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            curLevel.Update(gameTime);
+            if (level1.hero.IsAttacking)
+            {
+                Debug.WriteLine("attack");
+            }
+            else
+            {
+                Debug.WriteLine("stop");
+            }
+
+        }
+    }
+}
--- GameProject_GameDev/GameState/MenuState.cs
+++ GameProject_GameDev/GameState/MenuState.cs
@@ -1,12 +1,69 @@
-﻿using System;
+﻿using GameProject_GameDev.Button;
+using GameProject_GameDev.UI;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using SharpDX.Direct3D9;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
 
 namespace GameProject_GameDev.GameState
 {
-    internal class MenuState
+    internal class MenuState : State
     {
+
+
+        private StartButton startButton;
+        private QuitButton quitButton;
+
+        private Texture2D backgroundtexture;
+        List<Button.Button> buttons;
+        public MenuState(Game1 game, GraphicsDevice graphicsDevice, ContentManager content) : base(game, graphicsDevice, content)
+        {
+            buttons = new List<Button.Button>();
+             int Xpos = ScreenSettings.ScreenWidth / 2 - 50; // nog aanpassen
+             int Ypos = ScreenSettings.ScreenHeight / 2 - 15; // nog aanpassen
+            startButton = new StartButton(game, graphicsDevice, content, new Vector2(Xpos, Ypos), "Start");
+            quitButton = new QuitButton(game, graphicsDevice, content, new Vector2(Xpos, Ypos + 45),  "Quit");
+            buttons.Add(startButton);
+            buttons.Add(quitButton);
+            backgroundtexture = content.Load<Texture2D>("background"); 
+        }
+
+
+        public override void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(backgroundtexture, new Rectangle(0, 0, ScreenSettings.ScreenWidth, ScreenSettings.ScreenHeight), Color.White);
+
+            foreach (Button.Button item in buttons)
+            {
+                item.Draw(spriteBatch);
+            }
+        }
+
+        public override void LoadContent()
+        {
+
+            
+        }
+
+        public override void PostUpdate(GameTime gameTime)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            //startButton.Update(gameTime);
+            foreach (Button.Button item in buttons)
+            {
+                item.Update(gameTime);
+                
+            }
+        }
     }
 }
