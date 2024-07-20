4d0b031a21a320f8ff37bd210cbe10651bf6e2c9
alielyousfi02 <ali.elyousfi@student.ap.be>
2024-07-20 23:50:18 +0200

    Add Star and Add star logic in LevelState

---
+++ GameProject_GameDev/StarMap/Star.cs
@@ -0,0 +1,52 @@
+﻿using GameProject_GameDev.Players;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameProject_GameDev.StarMap
+{
+    internal class Star
+    {
+        private Texture2D texture;
+        private Vector2 position;
+        private bool IsCollected;
+        private Rectangle rectangle;
+        private Rectangle srcRec;
+        public Rectangle HitBox
+        {
+            get { return rectangle; }
+        }
+        public Star(Texture2D texture, Vector2 position)
+        {
+            this.texture = texture;
+            this.position = position;
+            IsCollected = false;
+            srcRec = new Rectangle(0, 0, texture.Width, texture.Height);
+            rectangle = new Rectangle((int)position.X, (int)position.Y, texture.Width, texture.Height);
+        }
+        public void Collect()
+        {
+            IsCollected = true;
+        }
+        public void Draw(SpriteBatch spriteBatch)
+        {
+
+            if (!IsCollected)
+            {
+                spriteBatch.Draw(texture, rectangle, SpriteRectangleBorder.GetNonTransparentBoundingBox(texture, srcRec), Color.White);
+            };
+        }
+
+        public void Update(GameTime time)
+        {
+            if (!IsCollected)
+            {
+
+            }
+        }
+    }
+}
